89ab7f8cda13101d5b636d5b724c209e3ae3624e
chadnelson101 <chadnelson279@gmail.com>
2023-11-30 15:45:05 +0200

    new commit

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
         sidebar.classList.toggle('active');
     });
 });
+
+let navlist = document.querySelectorAll('.navlist');
+
+// Function to handle the click event
+function activeLink() {
+    // Remove 'active' class from all elements
+    navlist.forEach((item) => item.classList.remove('active'));
+
+    // Add 'active' class to the clicked element
+    this.classList.add('active');
+}
+
+// Add click event listener to each element in navlist
+navlist.forEach((item) => item.addEventListener('click', activeLink));
